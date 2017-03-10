3bf53242c80edef2131e05f9d8d605906f0b747c
murata-yuka <murata-yuka@9115-077181.ad.imjp.co.jp>
2017-03-10 12:24:40 +0900

    タブ切り替えjsの追加

--- js/tabChange.js
+++ js/tabChange.js
@@ -1,11 +1,23 @@
 $(function () {
 
 	$('a[href^="#"]').click(function(){
-		var href = $(this).attr('href');
-		var content = $(href == "#" || href == "" ? 'href':href);
+		var target = $(this.hash);
+		var tab = $(this).parent();
 
-		$('.content').css('display', 'none');
-				$(href).css('display', 'block');
-});
+		tabColorChange(tab);
+		contentChange(target);
+
+	});
+
+	function tabColorChange(tab){
+		var tabs = $('.button p');
+		tabs.removeClass('on');
+		tab.addClass('on');
+	}
+
+	function contentChange(target){
+		$('.content').hide();
+		$(target).show();
+	}
 
 });
